54c4a6f389a18c2cc17c6a980decaab223d626cd
Marc <marc.chan89@gmail.com>
2022-05-29 17:48:56 +0800

    multi-resolution clustering now accepts passing of `member_counts` for multi-level clustering

--- openweb/thematic/clusters.py
+++ openweb/thematic/clusters.py
@@ -2,7 +2,7 @@
 Module to do multi-resolution clustering.
 """
 import logging
-from typing import Iterable, List, cast
+from typing import Iterable, List, Union, cast
 
 import numpy as np
 
@@ -25,10 +25,11 @@ class FastCommunityDetection:
         self,
         X,
         y=None,  # pylint: disable=unused-argument
+        member_counts: Union[List[int], np.ndarray] = None
     ) -> "FastCommunityDetection":
         self.labels_ = np.repeat(-1, len(X))
         cluster_centers = cast(List[int], [])
-        clusters = community_detection(X, **self._kwargs)
+        clusters = community_detection(X, member_counts=member_counts, **self._kwargs)
         for cluster_id, cluster in enumerate(clusters):
             cluster_centers.append(X[cluster[0]])
             for idx in cluster:
@@ -36,8 +37,8 @@ class FastCommunityDetection:
         self.cluster_centers_ = np.array(cluster_centers)
         return self
 
-    def fit_predict(self, X, y=None) -> np.ndarray:
-        return self.fit(X, y).labels_
+    def fit_predict(self, X, y=None, member_counts: Union[List[int], np.ndarray] = None) -> np.ndarray:
+        return self.fit(X, y, member_counts=member_counts).labels_
 
 
 class MultiResCommunityDetection:
@@ -60,11 +61,16 @@ class MultiResCommunityDetection:
         self,
         X,
         y=None,  # pylint: disable=unused-argument
+        member_counts: Union[List[int], np.ndarray] = None
     ) -> "MultiResCommunityDetection":
         self.labels_ = np.repeat(-1, len(X))
         cluster_centers = cast(List[int], [])
         mutable_x = np.array(X)
         indices = np.array(range(len(X)))
+        if mutable_member_counts is not None:
+            mutable_member_counts = np.array(member_counts).flatten()
+        else:
+            mutable_member_counts = np.ones(len(X))
         cluster_cnt = 0
         for idx, kwargs in enumerate(self._params):
             if len(mutable_x) == 0:
@@ -74,7 +80,7 @@ class MultiResCommunityDetection:
             model = self._factory(**kwargs)
             self._logger.info("created model #%s: %s", idx, kwargs)
 
-            labels = model.fit_predict(mutable_x)
+            labels = model.fit_predict(mutable_x, member_counts=mutable_member_counts)
             centers = model.cluster_centers_.tolist()
             self._logger.info("found %s clusters", len(centers))
 
@@ -87,5 +93,6 @@ class MultiResCommunityDetection:
             cluster_cnt += max(labels) + 1
             mutable_x = mutable_x[labels < 0]
             indices = indices[labels < 0]
+            mutable_member_counts = mutable_member_counts[labels < 0]
         self.cluster_centers_ = np.array(cluster_centers)
         return self
